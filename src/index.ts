ca190c70680819d9d3138d4c329c198eafd0e1e7
e7h4n <perfectworks@gmail.com>
2024-11-06 15:58:26 +0800

    refine code

--- src/index.ts
+++ src/index.ts
@@ -7,46 +7,58 @@ type RouterDecorator = (...args: createRouterArgs) => createRouterArgs;
 type DataFunctionValue = Response | NonNullable<unknown> | null;
 type DataFunctionReturnValue = Promise<DataFunctionValue> | DataFunctionValue;
 
+function fillLoader(route: RouteObject): RouteObject {
+    return Object.assign({}, route, {
+        loader: route.loader ?? true,
+        children: route.children?.map(fillLoader),
+    });
+}
+
+function loadAllRoutesWithSignal(createSignal: (match: DataStrategyMatch) => AbortSignal, matches: DataStrategyMatch[]) {
+    return Promise.all(matches
+        .map((match) => {
+            const signal = createSignal(match);
+            return match.resolve((handler: (ctx?: unknown) => DataFunctionReturnValue) => {
+                return handler({ signal })
+            })
+        }));
+}
+
+function createSignalForMatch(ctrlMap: Map<string, AbortController>, match: DataStrategyMatch, signal: AbortSignal) {
+    const ctrl = new AbortController();
+    ctrlMap.set(match.route.id, ctrl);
+    return AbortSignal.any([signal, ctrl.signal])
+}
+
+function abortForRoute(ctrlMap: Map<string, AbortController>, routeId: string) {
+    ctrlMap.get(routeId)?.abort();
+    ctrlMap.delete(routeId);
+}
+
+function abortOldControllers(ctrlMap: Map<string, AbortController>, matches: DataStrategyMatch[]) {
+    const matchRouteIds = new Set<string>(
+        matches.map((m) => m.route.id)
+    );
+    const ctrlsToRemove = Array.from(ctrlMap.keys()).filter(
+        (k) => !matchRouteIds.has(k)
+    );
+    ctrlsToRemove.forEach((k) => {
+        abortForRoute(ctrlMap, k);
+    });
+}
+
 export function createSignalRouterDecorator(signal: AbortSignal): RouterDecorator {
     const ctrlMap = new Map<string, AbortController>();
 
     return function (routes: RouteObject[], options: RouterOptions) {
-        const fillLoader = (route: RouteObject): RouteObject =>
-            Object.assign({}, route, {
-                loader: route.loader ?? true,
-                children: route.children?.map(fillLoader),
-            });
-
         return [
             routes.map(fillLoader),
             Object.assign({}, options, {
                 dataStrategy: async function ({ matches }: { matches: DataStrategyMatch[] }) {
-                    const matchRouteIds = new Set<string>(
-                        matches.map((m) => m.route.id)
-                    );
-                    const ctrlsToRemove = Array.from(ctrlMap.keys()).filter(
-                        (k) => !matchRouteIds.has(k)
-                    );
-                    ctrlsToRemove.forEach((k) => {
-                        ctrlMap.get(k)?.abort();
-                        ctrlMap.delete(k);
-                    });
+                    abortOldControllers(ctrlMap, matches)
 
-                    const matchesToLoad = matches.filter((m) => m.shouldLoad);
-                    const results = await Promise.all(
-                        matchesToLoad
-                            .map((match) => {
-                                const ctrl = new AbortController();
-                                ctrlMap.set(match.route.id, ctrl);
-                                const routeSignal = AbortSignal.any([
-                                    signal,
-                                    ctrl.signal,
-                                ]);
-                                return match.resolve((handler: (ctx?: unknown) => DataFunctionReturnValue) => {
-                                    return handler({ signal: routeSignal })
-                                })
-                            })
-                    );
+                    const matchesToLoad = matches.filter((m) => m.shouldLoad)
+                    const results = await loadAllRoutesWithSignal((match) => createSignalForMatch(ctrlMap, match, signal), matchesToLoad)
 
                     return results.reduce(
                         (acc: Record<string, unknown>, result: unknown, i: number) =>
